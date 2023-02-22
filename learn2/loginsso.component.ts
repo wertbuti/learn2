39b2f2da097b59801b70eedf5f901718afc66db6
wertbuti <103345246+wertbuti@users.noreply.github.com>
2023-02-22 06:05:17 +0330

    Create loginsso.component.ts

---
+++ learn2/loginsso.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+// import 'src/app/js/amar.js';
+
+@Component({
+  selector: 'app-loginsso',
+  templateUrl: './loginsso.component.html',
+  styleUrls: ['./loginsso.component.css']
+})
+export class LoginssoComponent {
+
+}
